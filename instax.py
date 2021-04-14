808c7575f57048068a516c90fe8b389db72dcc86
sserakal <sserakal@student.wethinkcode.co.za>
2021-04-14 13:23:00 +0200

    added supervision feature to pause likes and comments

--- instax.py
+++ instax.py
@@ -4,11 +4,10 @@ Import Instapy module
 from instapy import InstaPy
 
 from time import sleep
-
-
 """
 Delay execution while instagram loads login page
 """
+
 sleep(2)
 
 """
@@ -16,21 +15,28 @@ This feature allows the bot to run without the GUI of the browser.
 This is super useful if you want to deploy your bot to a server where you may not have
 or need the graphical interface. It’s also less CPU intensive, so it improves performance.
 """
-session = InstaPy(username='username', password='password', headless_browser=True)
 
+session = InstaPy(username='username', password='password', headless_browser=True)
+#session = InstaPy(username='datmemequeen42',password='keyaitaly')
+session.login()
 
 """
 Bot will only like posts based the specified tags below 
 """
-session.like_by_tags(["memes","mzansi"], amount = 5)
 #session.like_by_locations(['224442573/salton-sea/'], amount=100)
-session.like_by_locations(['214345561','1018622046'], amount=100)
-session.like_by_locations(locations=['ZA/south-africa/'], amount=100, media='Photo')
+session.like_by_tags(["selfcare","waisttrainer","workout","gym"], amount = 50,media='Photo')
+session.like_by_locations(['214345561','1018622046'], amount=50)
+#session.like_by_locations(locations=['ZA/south-africa/'], amount=100, media='Photo')
 
 """
 Set quota supervisor to prevent being banned by instagram
 """
-session.set_quota_supervisor(enabled=True, peak_comments_daily=240, peak_comments_hourly=21)
+session.set_quota_supervisor(enabled=True, sleep_after=['likes', 'comments_d', 'follows', 'unfollows', 'server_calls_h'], sleepyhead=True, stochastic_flow=True, notify_me=True,
+                              peak_likes=(57, 585),
+                               peak_comments=(21, 182),
+                                peak_follows=(48, None),
+                                 peak_unfollows=(35, 402),
+                                  peak_server_calls=(None, 4700))
 
 """
 This will close the browser, save the logs, 
